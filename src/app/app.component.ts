c42eca76a4da66e728062c0dec84d8b96f501cd8
Johnny Gérard <john@jgerard.dev>
2024-04-03 17:16:19 +0200

    feat: set up ImageKit and add content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    NgOptimizedImage,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
